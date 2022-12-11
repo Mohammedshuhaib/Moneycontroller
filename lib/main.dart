7a7ce7e0b08aebd867e172fdcafc5607bd010eb8
shuhaib-inunity <mohammed.shuhaib@inunity.in>
2022-12-11 15:35:28 +0530

    added text fields and transaction history

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,17 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: 'New shoes', amount: 699.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: 'New watch', amount: 690.99, date: DateTime.now()),
+    Transaction(
+        id: "t3", title: 'New earbuds', amount: 190.99, date: DateTime.now()),
+    Transaction(
+        id: "t4", title: 'New shirt', amount: 109.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +33,70 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
               color: Colors.blue,
-              child: Container(width: double.infinity, child: Text("CHART")),
+              child:
+                  Container(width: double.infinity, child: const Text("CHART")),
               elevation: 5,
             ),
             Card(
-              child: Text("List of Tx"),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextField(
+                          decoration: InputDecoration(labelText: "title")),
+                      TextField(
+                        decoration: InputDecoration(labelText: "amount"),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.purple,
+                                  foregroundColor: Colors.white),
+                              onPressed: () {},
+                              child: Text("Add transactions")))
+                    ]),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text('₹${tx.amount}',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          )),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          DateFormat.yMMMd().format(tx.date),
+                          style: const TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ]),
+                );
+              }).toList(),
             )
           ],
         ));
